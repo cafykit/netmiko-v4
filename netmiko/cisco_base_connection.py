be7686f7f07b2d23acb4d969b6b71594da602fa5
Shreya Shah <shreyash@cisco.com>
2024-06-19 21:03:03 -0700

    Pass ctrl-q char to unlock it from telnet hung state

--- netmiko/cisco_base_connection.py
+++ netmiko/cisco_base_connection.py
@@ -123,6 +123,9 @@ class CiscoBaseConnection(BaseConnection):
                     if cxr_pattern in prompt.lower():
                         time.sleep((delay_factor * 0.1) + 3)
                         prompt = self.read_channel()
+                    if count == 5 and not prompt:
+                        self.log.info("Probably in telnet hung state. Passing ctrl-q . Potential DDTS: CSCwh67759 and CSCvy64395")
+                        self.write_channel('\x11')
                 count += 1
 
         # If multiple lines in the output take the last line
